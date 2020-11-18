367c13b87de8f8b27118ae57d819f8c255abc25c
Nkateko00 <tekonku@icloud.com>
2020-11-18 16:17:51 +0200

    js-function+tests

--- test/waiters.test.js
+++ test/waiters.test.js
@@ -34,8 +34,6 @@ it("should add waiters Names onto database",async function(){
     await waiter.addNames('Shane');
     await waiter.addNames('Kagiso');
 
-    const allWaiters = await waiter.allWaiters();
-
     assert.deepEqual([{waiters : 'Themba'}],[{waiters : 'Teko'}], 
     [{waiters : 'Shane'}],[{waiters : 'Kagiso'}],await waiter.allWaiters());
 
@@ -46,8 +44,21 @@ it('should not add duplicate names onto the database',async function(){
     await waiter.addNames('Teko');
     await waiter.addNames('Mihlali');
     await waiter.addNames('Mihlali');
-
-    assert.deepEqual([{waiters : 'Teko'},{ waiters : 'Mihlali'}])
+    assert.deepEqual([{waiters : 'Teko'},{ waiters : 'Mihlali'}],await waiter.allWaiters());
 
 });
+it('should return all the Weekdays',async function(){
+
+    await waiter.addWeekdays('Monday');
+    await waiter.addWeekdays('Tuesday');
+    await waiter.addWeekdays('Wednesday');
+    await waiter.addWeekdays('Thursday');
+    await waiter.addWeekdays('Friday');
+    await waiter.addWeekdays('Saturday');
+    await waiter.addWeekdays('Sunday');
+
+    assert.deepEqual([{ weekdays : 'Monday'},{weekdays : 'Tuesday'},,{weekdays : 'Wednesday'},,{weekdays : 'Thursday'}
+    ,{weekdays : 'Friday'},{weekdays : 'Saturday'},{weekdays : 'Sunday'}],await waiter.getWeekdays());
+})
+
 });
--- waiters.js
+++ waiters.js
@@ -1,28 +1,37 @@
 module.exports = function (pool) {
 
-    async function addName(name){
-        if(!name == ""){
+    async function addName(name) {
+        if (!name == "") {
             var specialCase = /[^A-Za-z]/g;
-            var noNumbers = name.replace(specialCase,"");
+            var noNumbers = name.replace(specialCase, "");
             var nameEntered = noNumbers.charAt(0).toUpperCase() + noNumber.slice(1).toLowerCase();
 
-            const verified = await pool.query(`select id from Waiters where waiters = $1`,[nameEntered])
+            const verified = await pool.query(`select id from Waiters where waiters = $1`, [nameEntered])
 
-            if(verified.rowCount === 0){
+            if (verified.rowCount === 0) {
 
-            await pool.query(`insert into Waiters waiters values ($1)`, [nameEntered]);
-            return 'Name Entered!'
-        }
-        return 'Name was already entered'
+                await pool.query(`insert into Waiters waiters values ($1)`, [nameEntered]);
+                return 'Name Entered!'
+            }
+            return 'Name was already entered'
         }
     }
+    async function addWeekdays(){
+        
+    }
+    async function getWeekdays() {
 
-    async function allWaiters(){
+        const allWeekdays = await pool.query(`select weekdays from Weekdays`);
+        return allWeekdays.rows;
+
+    }
+
+    async function allWaiters() {
         const allWaiters = await pool.query(`select waiters from Waiters`);
         return allWaiters.rows;
     }
 
-    async function clear(){
+    async function clear() {
         const remove = await pool.query(`delete from Timesheet`);
         return remove.rows();
 
@@ -31,6 +40,8 @@ module.exports = function (pool) {
         clear,
         addName,
         allWaiters,
-        
+        getWeekdays,
+        addWeekdays
+
     }
- }
+}
